8870884d7f1aa76756207eb7f25356de14706a20
Xavivo <albertovazquezmonge@gmail.com>
2025-10-13 13:19:10 +0200

    Settling of project structure

---
+++ Alcoholdle/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Alcoholdle</h1>
+      <p>Welcome to Alcoholdle, a fun twist on the classic word-guessing game! In this game, your goal is to guess the name of a popular alcoholic beverage within six attempts. Each guess must be a valid drink name, and after each guess, you'll receive feedback to help you get closer to the correct answer.</p>
+    </div>
+  )
+}
+
+export default App
